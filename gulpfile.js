9740409c73b2b1e7732e4fab2b7272e2c26a6052
Michal Jeřábek <mjerabek@seznam.cz>
2017-06-05 17:08:06 +0200

    npm + gulp

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+/*jslint indent: 4, white: true, nomen: true, regexp: true, unparam: true, node: true, browser: true, devel: true, nomen: true, plusplus: true, regexp: true, sloppy: true, vars: true*/
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var cleanCSS = require("gulp-clean-css");
+
+gulp.task("js", function () {
+
+    gulp.src(["./src/*.js"])
+        .pipe(concat("infinitum.dist.js"))
+        .pipe(
+            gulp.dest("./dist")
+        )
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.basename = "infinitum.dist.min";
+        }))
+        .pipe(
+            gulp.dest("./dist")
+        );
+});
+
+gulp.task("css", function () {
+
+    gulp.src(["./src/*.css"])
+        .pipe(concat("infinitum.css"))
+        .pipe(
+            gulp.dest("./dist/css")
+        )
+        .pipe(cleanCSS())
+        .pipe(rename(function (path) {
+            path.basename = "infinitum.dist.min";
+        }))
+        .pipe(
+            gulp.dest("./dist/css")
+        );
+});
+
+gulp.task("watch", function () {
+
+    gulp.watch(["src/**/*.js", "src/*.js"], ["js"]);
+    gulp.watch(["src/*.css"], ["css"]);
+});
