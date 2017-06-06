36a08fe6e5a9cd8e0402aedb1226934e35a923aa
Michal Jeřábek <mjerabek@seznam.cz>
2017-06-06 12:00:26 +0200

    Transitions check element and property; Last item moves to the start if there is place; It respects paddings and borders;

--- src/Infinitum.js
+++ src/Infinitum.js
@@ -153,11 +153,11 @@
         this.startItemPos = 0;
         this.endItemPos = 0;
 
-        this.$startItem = $();
-        this.$endItem = $();
-        this.$leftItemsOver = $();
-        this.$rightItemsOver = $();
-        this.$insideItems = $();
+//        this.$startItem = $();
+//        this.$endItem = $();
+//        this.$leftItemsOver = $();
+//        this.$rightItemsOver = $();
+//        this.$insideItems = $();
 
         this.init(options);
     };
@@ -177,23 +177,11 @@
 
         this.options = $.extend({}, this.options);
 
-        this.$self = $(this.options.selector)
-            .addClass(CLASS.self);
+        this._prepareSelf();
 
-        this.$track = $(this.$self).children()
-            .addClass(CLASS.track);
-
-        this.$track.css({
-            minHeight: this.$track.outerHeight(),
-            position: "relative"
-        });
-
-        this.$items = $(this.$track).children()
-            .addClass(CLASS.item);
+        this._prepareTrack();
 
         this._prepareItems();
-
-        this.selfRect = this.$self[0].getBoundingClientRect();
 
         this._initEvents();
 
@@ -206,8 +194,6 @@
         this._shouldCancelRAF = true;
 
         this.$track.css(TRANSITION_PROP, "none");
-
-        this.$items.removeClass(CLASS.current);
 
         this._lastClinetX = event.clientX;
 
@@ -230,6 +216,8 @@
     Infinitum.prototype._onPointerEnd = function () {
 
         var closestItem = this._findClosestItem();
+
+        this.$track.children().removeClass(CLASS.current);
 
         closestItem.$el.addClass(CLASS.current);
 
@@ -255,9 +243,9 @@
 
             var left = item.getBoundingClientRect().left;
 
-            if (closestLeftItemPos === null || Math.abs(closestLeftItemPos) > Math.abs(left - this.selfRect.left)) {
+            if (closestLeftItemPos === null || Math.abs(closestLeftItemPos) > Math.abs(left - this._selfRect.left)) {
 
-                closestLeftItemPos = left - this.selfRect.left;
+                closestLeftItemPos = left - this._selfRect.left;
 
                 $closestLeftItem = $(item);
             }
@@ -279,7 +267,34 @@
         });
     };
 
+    Infinitum.prototype._prepareSelf = function () {
+
+        this.$self = $(this.options.selector)
+            .addClass(CLASS.self);
+
+        this._selfRect = {};
+        this._origSelfRect = this.$self[0].getBoundingClientRect();
+
+        var box = this.$self.css(["padding-left", "padding-right", "border-left-width", "border-right-width"]);
+
+        this._selfRect.left = this._origSelfRect.left + parseFloat(box["padding-left"]) + parseFloat(box["border-left-width"]);
+        this._selfRect.right = this._origSelfRect.right - parseFloat(box["padding-right"]) - parseFloat(box["border-right-width"]);
+    };
+
+    Infinitum.prototype._prepareTrack = function () {
+
+        this.$track = $(this.$self).children()
+            .addClass(CLASS.track);
+
+        this.$track.css({
+            minHeight: this.$track.css("height")
+        });
+    };
+
     Infinitum.prototype._prepareItems = function () {
+
+        this.$items = $(this.$track).children()
+            .addClass(CLASS.item);
 
         var lastLeft = 0;
 
@@ -376,9 +391,14 @@
                     $prev = _this.$items.last();
                 }
 
+                var opacity = $this.css("opacity");
+
                 $this.removeClass(CLASS.hide);
 
                 var $clone = $this.clone();
+
+                $clone.css("transition", "none");
+                $clone.css("opacity", opacity);
 
                 $clone.addClass(CLASS.clone);
 
@@ -400,7 +420,12 @@
 
                 var fakeTransitionEnd,
 
-                    onTransitionend = function () {
+                    onTransitionend = function (event) {
+
+                        if (event && event.originalEvent.target !== $clone[0] && event.originalEvent.propertyName !== "opacity") {
+
+                            return;
+                        }
 
                         clearTimeout(fakeTransitionEnd);
 
@@ -411,13 +436,24 @@
                             $this.removeClass(CLASS.hide);
                             $this.removeClass("left");
                         }
+
+                        $clone.off(TRANSITIONEND);
                     };
 
-                fakeTransitionEnd = setTimeout(onTransitionend, 1050);
+                if (!parseFloat(opacity)) {
 
-                $clone.one(TRANSITIONEND, onTransitionend);
+                    onTransitionend();
 
-                $clone.addClass(CLASS.hide);
+                } else {
+
+                    fakeTransitionEnd = setTimeout(onTransitionend, 1050);
+
+                    $clone.on(TRANSITIONEND, onTransitionend);
+
+                    $clone.css("transition", "");
+                    $clone.addClass(CLASS.hide);
+                    $clone.css("opacity", "");
+                }
             }
         });
     };
@@ -432,7 +468,7 @@
 
             var $this = $(this);
 
-            if (_this.startItemPos - addedWidth <= _this.selfRect.left) {
+            if (_this.startItemPos - addedWidth <= _this._selfRect.left) {
 
                 return;
             }
@@ -448,9 +484,14 @@
 
                 addedWidth += $this.outerWidth();
 
+                var opacity = $this.css("opacity");
+
                 $this.removeClass(CLASS.hide);
 
                 var $clone = $this.clone();
+
+                $clone.css("transition", "none");
+                $clone.css("opacity", opacity);
 
                 $clone.addClass(CLASS.clone);
 
@@ -472,7 +513,12 @@
 
                 var fakeTransitionEnd,
 
-                    onTransitionend = function () {
+                    onTransitionend = function (event) {
+
+                        if (event && event.originalEvent.target !== $clone[0] && event.originalEvent.propertyName !== "opacity") {
+
+                            return;
+                        }
 
                         clearTimeout(fakeTransitionEnd);
 
@@ -483,13 +529,24 @@
                             $this.removeClass(CLASS.hide);
                             $this.removeClass("right");
                         }
+
+                        $clone.off(TRANSITIONEND);
                     };
 
-                fakeTransitionEnd = setTimeout(onTransitionend, 1050);
+                if (!parseFloat(opacity)) {
 
-                $clone.one(TRANSITIONEND, onTransitionend);
+                    onTransitionend();
 
-                $clone.addClass(CLASS.hide);
+                } else {
+
+                    fakeTransitionEnd = setTimeout(onTransitionend, 1050);
+
+                    $clone.on(TRANSITIONEND, onTransitionend);
+
+                    $clone.css("transition", "");
+                    $clone.addClass(CLASS.hide);
+                    $clone.css("opacity", "");
+                }
             }
         });
 
@@ -503,7 +560,8 @@
         var leftItemsOver = [],
             rightItemsOver = [],
             startItem = null,
-            endItem = null;
+            endItem = null,
+            startItemRect = null;
 
         this.$insideItems = $([]);
 
@@ -516,6 +574,8 @@
                 this.startItemPos = rect.left;
 
                 startItem = item;
+
+                startItemRect = rect;
             }
 
             if (this.endItemPos === null || this.endItemPos < rect.right) {
@@ -525,11 +585,11 @@
                 endItem = item;
             }
 
-            if (rect.left < this.selfRect.left) {
+            if (rect.left < this._selfRect.left) {
 
                 leftItemsOver.push(item);
 
-            } else if (rect.right > this.selfRect.right) {
+            } else if (rect.right > this._selfRect.right) {
 
                 rightItemsOver.push(item);
 
@@ -552,6 +612,11 @@
 
             return b.getBoundingClientRect().left - a.getBoundingClientRect().left;
         }));
+
+        if (!this.$leftItemsOver.length && !this.$rightItemsOver.length && startItemRect.left > this._selfRect.left) {
+
+            this.$rightItemsOver = this.$endItem;
+        }
     };
 
     Infinitum.prototype._animate = function () {
@@ -595,363 +660,3 @@
     };
 
 }(jQuery));
-/*
-var lastTrans = 0, cancel = false;
-
-var move = function (move, animation) {
-
-    var linksLeft = $links[0].getBoundingClientRect().left;
-    var linksRight = $links[0].getBoundingClientRect().right;
-
-    speed = speed.slice(0, 2);
-
-    speed.unshift(Math.abs(move));
-
-    var avgSpeed = speed.reduce(function (prev, current) {
-        return prev + current;
-    }, 0) / speed.length;
-
-    var duration = 500 / (Math.log(avgSpeed) || 1);
-
-    if (Math.log(avgSpeed) < 1) {
-
-        $linksWrapper.removeClass("medium").removeClass("fast").addClass("slow");
-
-    } else if (Math.log(avgSpeed) < 2) {
-
-        $linksWrapper.removeClass("slow").removeClass("fast").addClass("medium");
-
-    } else {
-
-        $linksWrapper.removeClass("medium").removeClass("slow").addClass("fast");
-
-    }
-
-    if (!animation) {
-
-        $linksWrapper.css("transform", `translateX(${(getTranslate($linksWrapper).x + move)}px)`);
-
-    } else if (!cancel) {
-
-        requestAnimationFrame(anim);
-    }
-
-    var start = null,
-        end = null,
-        $start = null,
-        $end = null,
-        $leftOver = $([]),
-        $rightOver = $([]),
-        $middle = $([]);
-
-    $linksWrapper.children(":not(.removing)").each(function () {
-
-        var $this = $(this),
-
-            left = this.getBoundingClientRect().left,
-            right = this.getBoundingClientRect().right;
-
-        if (start === null || start > left) {
-
-            start = left;
-
-            $start = $this;
-        }
-
-        if (end === null || end < right) {
-
-            end = right;
-
-            $end = $this;
-        }
-
-        if (left < linksLeft) {
-
-            $leftOver.push(this);
-
-        } else if (right > linksRight) {
-
-            $rightOver.push(this);
-
-        } else {
-
-            $middle.push(this);
-        }
-
-    });
-
-    $leftOver = $($leftOver.get().sort(function (a, b) {
-
-        return a.getBoundingClientRect().left - b.getBoundingClientRect().left;
-    }));
-
-    $rightOver = $($rightOver.get().sort(function (a, b) {
-
-        return b.getBoundingClientRect().left - a.getBoundingClientRect().left;
-    }));
-
-    var currentIsNotFirst = (animation && cancel && $start.length && !$start.hasClass("current"));
-
-    //left
-    if (move < 0 || currentIsNotFirst) {
-
-        $leftOver.filter(currentIsNotFirst ? ":not(.current)" : "*").each(function (i) {
-
-            var $this = $(this);
-
-            if (!$this.hasClass("hide-tab-link-anim") || currentIsNotFirst) {
-
-                var $prev = $this.prevAll(":not(.removing)").first();
-
-                if (!$prev.length) {
-
-                    $prev = $linksWrapper.children(":not(.removing)").last();
-                }
-
-                var $clone = $this.clone();
-
-                $this.addClass("hide-tab-link-anim");
-
-                $clone.addClass("clone");
-
-                $this.after($clone);
-
-                $clone.after($this);
-
-                $this.css({
-                    left: parseFloat($prev.css("left")) + $prev.outerWidth()
-                });
-
-                var to = setTimeout(function() {
-
-                    $clone.remove();
-
-                    $this.removeClass("hide-tab-link-anim");
-                }, 1050);
-
-                $clone.one("transitionend", function () {
-
-                    clearTimeout(to);
-
-                    $clone.remove();
-                    $this.removeClass("hide-tab-link-anim");
-                });
-
-                $clone.addClass("hide-tab-link-anim");
-                $clone.addClass("removing");
-            }
-        });
-    }
-
-    //right
-    if (move > 0) {
-
-        var added = 0;
-
-        $rightOver.each(function (i) {
-
-            var $this = $(this);
-
-            if (start - added <= linksLeft) {
-
-                return;
-            }
-
-            if (!$this.hasClass("hide-tab-link-anim")) {
-
-                var $next = $this.nextAll(":not(.removing)").first();
-
-                if (!$next.length) {
-
-                    $next = $linksWrapper.children(":not(.removing)").first();
-                }
-
-                added += $this.outerWidth();
-
-                var $clone = $this.clone();
-
-                $this.addClass("hide-tab-link-anim");
-
-                $clone.addClass("clone");
-
-                $this.before($clone);
-
-                $clone.before($this);
-
-                $this.css({
-                    left: parseFloat($next.css("left")) - $this.outerWidth()
-                });
-
-                var to = setTimeout(function() {
-
-                    $clone.remove();
-                    $this.removeClass("hide-tab-link-anim");
-                }, 1050);
-
-                $clone.one("transitionend", function () {
-
-                    clearTimeout(to);
-
-                    $clone.remove();
-                    $this.removeClass("hide-tab-link-anim");
-                });
-
-                $clone.addClass("hide-tab-link-anim");
-                $clone.addClass("removing");
-            }
-        });
-
-    }
-};
-
-var getCurrentMatrix = function ($element) {
-
-    var currentTransform = $element.css("transform");
-
-    currentTransform = currentTransform === "none" || !currentTransform ? "matrix(1, 0, 0, 1, 0, 0)" : currentTransform;
-
-    var matrix = currentTransform.replace(/^.*\((.*)\)$/g, "$1").split(/, +/),
-
-        isMatrix3d = currentTransform.indexOf("3d") !== -1;
-
-    return {
-        value: matrix,
-        is3d: isMatrix3d
-    };
-},
-
-    getTranslate = function ($element) {
-
-        var matrix = getCurrentMatrix($element);
-
-        return {
-            x: (matrix.is3d ? +matrix.value[12] : +matrix.value[4]),
-            y: (matrix.is3d ? +matrix.value[13] : +matrix.value[5])
-        };
-    };
-
-var $links = $(".tab-links");
-var $link = $(".tab-link");
-var $linksWrapper = $(".tab-links-wrapper");
-
-var speed = [10, 10, 10];
-
-$linksWrapper.css({
-    position: "relative"
-});
-
-$link.each(function (i) {
-
-    $(this).css({
-        minWidth: $links.outerWidth() / $link.length,
-        width: (Math.random() * 200) + 100
-    });
-});
-
-var last = 0;
-
-$link.each(function (i) {
-
-    $(this).css({
-        position: "absolute",
-        left: last
-    });
-
-    $(this).data("left", last);
-
-    last += $(this).outerWidth();
-});
-
-$links.on("mousedown", function ({clientX}) {
-
-    $link = $linksWrapper.children(":not(.removing)");
-
-    cancelAnimationFrame(anim);
-    cancel = true;
-
-    $linksWrapper.css("transition", "none");
-
-    $link.removeClass("current");
-
-    var lastX = clientX;
-
-    var linksLeft = $links[0].getBoundingClientRect().left;
-    var linksRight = $links[0].getBoundingClientRect().right;
-
-    var lastDir = clientX - lastX > 0;
-
-
-    $(window).on("mousemove", function ({clientX, type}) {
-
-        move(clientX - lastX);
-
-        lastX = clientX;
-
-        return false;
-    });
-
-    $(window).one("mouseup", function () {
-
-        var closest = null,
-            $closest,
-
-            $sorted = $($link.get().sort(function (a, b) {
-
-                return a.getBoundingClientRect().left - b.getBoundingClientRect().left;
-            }));
-
-        var endLeft = parseFloat($sorted.last().css("left"));
-
-        $link = $linksWrapper.children(":not(.removing)");
-
-        $link.each(function (i) {
-
-            var $this = $(this),
-
-                left = this.getBoundingClientRect().left;
-
-            if (closest === null || Math.abs(closest) > Math.abs(left - linksLeft)) {
-
-                closest = left - linksLeft;
-
-                $closest = $this;
-            }
-        });
-
-        $closest.addClass("current");
-
-        $(window).off("mousemove mouseup");
-
-        if (!closest) {
-
-            console.info("not-moved");
-
-            return;
-        }
-
-        lastTrans = getTranslate($linksWrapper).x;
-
-        $linksWrapper.css("transition", "");
-
-        cancel = false;
-
-        $linksWrapper.one("transitionend", function () {
-            cancelAnimationFrame(anim);
-
-            cancel = true;
-        });
-
-        $linksWrapper.css("transform", `translateX(${(getTranslate($linksWrapper).x - closest)}px)`);
-
-        requestAnimationFrame(anim);
-    });
-
-    return false;
-});
-
-function anim () {
-
-    move(getTranslate($linksWrapper).x - lastTrans, true);
-
-    lastTrans = getTranslate($linksWrapper).x;
-}*/
